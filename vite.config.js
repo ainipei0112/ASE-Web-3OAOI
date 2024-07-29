3c7ae30e7a531e3d7f8b5885ca2a9704b38f0f87
ainipei0112 <ainipei0112@gmail.com>
2024-07-29 11:05:37 +0800

    0729 更新打包設定

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { // 加上這段將 port 指定為 3000
+  base: "./",
+  plugins: [
+    react(),
+  ],
+  server: {
+    // port 指定為 3000
     port: 3000,
   }
-})
+});
