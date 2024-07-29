7d5ae4d1137fb391cc5d5e8893e12b2859d80492
ainipei0112 <ainipei0112@gmail.com>
2024-07-29 16:55:42 +0800

    0729 新增 Autocomplete

--- src/Actions.jsx
+++ src/Actions.jsx
@@ -17,28 +17,13 @@ const fetchData = async (url, method, body) => {
 };
 
 const initialState = {
-  users: [],
-  products: [],
-  cachedProducts: {},
   airesults: [],
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "SET_USERS":
-      return { ...state, users: action.payload };
-    case "SET_PRODUCTS":
-      return { ...state, products: action.payload };
     case "SET_AIRESULT":
       return { ...state, airesults: action.payload };
-    case "CACHE_PRODUCTS":
-      return {
-        ...state,
-        cachedProducts: {
-          ...state.cachedProducts,
-          [action.payload.id]: action.payload.data,
-        },
-      };
     default:
       return state;
   }
@@ -47,66 +32,13 @@ const reducer = (state, action) => {
 const Actions = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const searchProduct = async (productId) => {
-    const getCachedProducts = () => {
-      if (state.cachedProducts[productId]) {
-        return state.cachedProducts[productId];
-      }
-
-      const filteredProducts = Object.values(state.cachedProducts).flatMap(
-        (products) =>
-          products.filter((product) => {
-            return (
-              product.lot.includes(productId) &&
-              productId.length === product.lot.length
-            );
-          })
-      );
-
-      return filteredProducts.length > 0 ? filteredProducts : null;
-    };
-
-    // 先檢查是否已經存在於cachedData中，如果存在，直接回傳暫存資料。
-    const cachedData = getCachedProducts();
-    if (cachedData) {
-      dispatch({ type: "SET_PRODUCTS", payload: cachedData });
-      return cachedData;
-    }
-
-    // 如果不存在，則從資料庫中取得資料
-    try {
-      const data = await fetchData(
-        "http://10.11.33.122:1234/all-data.php",
-        "POST",
-        {
-          action: "getProductById",
-          productId,
-        }
-      );
-      if (data.length > 0) {
-        dispatch({ type: "CACHE_PRODUCTS", payload: { id: productId, data } });
-        dispatch({ type: "SET_PRODUCTS", payload: data });
-        return data;
-      } else if (data.length === 0) {
-        console.warn("沒有匹配的商品資料");
-        dispatch({ type: "SET_PRODUCTS", payload: data });
-        return data;
-      }
-    } catch (err) {
-      console.error(err.message);
-      throw new Error("商品搜尋失敗");
-    }
-  };
-
   const searchAiresult = async (selectedCustomer, selectedDateRange) => {
     try {
       const data = await fetchData(
-        "http://10.11.33.122:1234/all-data.php",
+        "http://10.11.33.122:1234/3o-data.php",
         "POST",
         {
           action: "getAIResults",
-          selectedCustomer,
-          selectedDateRange,
         }
       );
       if (data.length > 0) {
@@ -124,9 +56,7 @@ const Actions = () => {
 
   // 回傳所有API抓取到的資料
   return {
-    products: state.products,
     airesults: state.airesults,
-    searchProduct,
     searchAiresult,
   };
 };
