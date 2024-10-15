a970b68253d63e5222c555e02b9e5c5d6f9486b7
ainipei0112 <ainipei0112@gmail.com>
2024-10-15 10:55:40 +0800

    1015 修正 React Fast Refresh Bug
    Fast refresh only works when a file only exports components.

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,4 +4,4 @@ import React from 'react'
 export const AppContext = React.createContext()
 
 // 使用 React.createContext 函式庫的 Provider 來建立一個名為 Provider 的組件，用來包裝應用程式的其他組件。
-export const { Provider } = AppContext
+export const Provider = AppContext.Provider
